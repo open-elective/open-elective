493ef2bcd0733f8294c7cbc937541797e593fca5
niranjangirhe <niranjangirheindia@gmail.com>
2021-06-02 18:11:34 +0530

    Minor fixes auth.js

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,29 +1,30 @@
 async function signup(e) {
-e.preventDefault()
-const email = document.getElementById("signinemail")
-const PRN = document.getElementById("signinPRN")
-var password = document.getElementById("signinpassword");
-try {
-    if (email.value.slice(-13) != "@mitaoe.ac.in") {
-        throw {
-            message:  "Invalid Mail-id",
-            error: new Error()
-        };
+    e.preventDefault()
+    const email = document.getElementById("signinemail")
+    const PRN = document.getElementById("signinPRN")
+    const password = document.getElementById("signinpassword");
+    try {
+        if (email.value.slice(-13) != "@mitaoe.ac.in") {
+            throw {
+                message:  "Invalid Mail-id",
+                error: new Error()
+            };
+        }
+        const result = await firebase.auth().createUserWithEmailAndPassword(email.value , password.value)
+        sendVerificationEmail()
+        await result.user.updateProfile({
+            displayName: "User"
+        });
+        PRN.value = ""
+        email.value = ""
+        password.value = ""
     }
-    else
-    {
-        password = Math.random().toString(36).slice(-8);        
+    catch (err) {
+        if (err.code == "auth/email-already-in-use") {
+            err.message = "Email already exist"
+        }
+        window.alert(err.message);
     }
-    const result = await firebase.auth().createUserWithEmailAndPassword(email.value , PRN.value)
-    sendVerificationEmail()
-}
-catch (err) {
-    if (err.code == "auth/email-already-in-use") {
-        err.message = "Email already exist"
-    }
-    window.alert(err.message);
-}
-
 }
 const sendVerificationEmail = () => {
     firebase.auth().currentUser.sendEmailVerification()
