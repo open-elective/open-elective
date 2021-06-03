4ce7b7ae9c5a3defd98b6828dd2abb24a7ec4c79
niranjangirhe <niranjangirheindia@gmail.com>
2021-06-03 15:12:01 +0530

    Added add admin feature

---
+++ scripts/adminsettings.js
@@ -0,0 +1,63 @@
+async function addadmin(e)
+{
+    e.preventDefault()
+    const email = document.getElementById("adminemail")
+    const Name = document.getElementById("adminname")
+    try {
+        // if (email.value.slice(-13) != "@mitaoe.ac.in") {
+        //     throw {
+        //         message: "Invalid Mail-id (Use official mail-id)",
+        //         error: new Error()
+        //     };
+        // }
+        // if (PRN.value.length != 10) {
+        //     throw {
+        //         message: "Invalid PRN (PRN should be 10 digits)",
+        //         error: new Error()
+        //     };
+        // }
+        //var progress = document.getElementById("addadmin");
+        var btn = document.getElementById("addadmin");
+        //progress.style.visibility = "visible";
+        btn.style.visibility = "hidden";
+        const result = await firebase.auth().createUserWithEmailAndPassword(email.value, "open_elective")
+        sendVerificationEmail()
+        await result.user.updateProfile({
+            displayName: "Admin",
+            photoURL: Name.value
+        });
+        Name.value = ""
+        email.value = ""
+        //progress.style.visibility = "hidden";
+        btn.style.visibility = "visible";
+        window.alert("Admin Added Successfully. (Please check inbox for verification)")
+        logout()
+    }
+    catch (err) {
+        if (err.code == "auth/email-already-in-use") {
+            err.message = "Email already exist"
+        }
+        window.alert(err.message);
+        //progress.style.visibility = "hidden";
+        btn.style.visibility = "visible";
+    }
+}
+const sendVerificationEmail = () => {
+     firebase.auth().currentUser.sendEmailVerification()
+        .then(() => {
+            console.log('Verification Email Sent Successfully !');
+            console.log(firebase.auth().currentUser);
+        })
+        .catch(error => {
+            console.error(error);
+        })
+}
+//log out
+function logout() {
+    firebase.auth().signOut().then(() => {
+        window.alert("Logout successfull")
+        window.location.href = "index.html";
+    }).catch((error) => {
+        window.alert(error.message)
+    });
+}
