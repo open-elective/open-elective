33f555a036ececa586a45d620c6ffde6737bd4b5
niranjangirhe <niranjangirheindia@gmail.com>
2021-06-05 18:53:02 +0530

    add desc order table

--- scripts/studdata.js
+++ scripts/studdata.js
@@ -1,24 +1,38 @@
 
-window.addEventListener('DOMContentLoaded', () => getdata());
+window.addEventListener('DOMContentLoaded', () => getdata(2));
 var input = document.getElementById('exl');
 var lastdoc = null;
 var nextpg = document.getElementById('page-next');
 var pgno = 1;
+var firstdoc = null;
+
 
 async function getdata(b) {
-    var rows = document.getElementById("courset").rows.length;
+    var rows = document.getElementById("studdatat").rows.length;
+    //page length
     var pglen = 30;
+
+    //delete all row
     for (i = 2; i < rows; i++)
-        document.getElementById("courset").deleteRow(1);
+        document.getElementById("studdatat").deleteRow(1);
     var data = 0;
+
+    //get highest marking student
+    await db.collection("studentData").orderBy("CGPA", "desc").limit(1).get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            firstdoc = doc;
+        });
+    });
+
+    //next page
     if (b == 1) {
-        const ref = await db.collection("studentData").orderBy("CGPA").startAfter(lastdoc || 0).limit(pglen);
+        const ref = await db.collection("studentData").orderBy("CGPA", "desc").startAfter(lastdoc).limit(pglen);
         data = await ref.get();
         pgno++;
         document.getElementById("page").innerHTML = "Page " + pgno.toString();
     }
     else {
-        const ref = await db.collection("studentData").orderBy("CGPA").startAfter(0).limit(pglen);
+        const ref = await db.collection("studentData").orderBy("CGPA", "desc").startAt(firstdoc).limit(pglen);
         data = await ref.get();
         pgno = 1;
         document.getElementById("page").innerHTML = "Page " + pgno.toString();
@@ -27,7 +41,10 @@ async function getdata(b) {
         const d = doc.data();
         addStudentDataTable(doc.id, d.Name, d.CGPA, d.Branch);
     });
+
+    //note the last row data for paging
     lastdoc = data.docs[data.docs.length - 1]
+
     if (data.empty || data.docs.length < pglen) {
         nextpg.className = "waves-effect waves-light btn blue darken-2 disabled";
     }
@@ -116,8 +133,8 @@ async function uploaddata(data) {
 }
 
 function addStudentDataTable(prn, name, cgpa, school) {
-    var table = document.getElementById("courset");
-    var row = table.insertRow(1);
+    var table = document.getElementById("studdatat");
+    var row = table.insertRow(document.getElementById("studdatat").rows.length);
     var prnt = row.insertCell(0);
     var namet = row.insertCell(1);
     var cgpat = row.insertCell(2);
@@ -134,9 +151,9 @@ function searchtest() {
         db.collection("studentData").doc(res.toString())
             .get().then((doc) => {
                 if (doc.exists) {
-                    var rows = document.getElementById("courset").rows.length;
+                    var rows = document.getElementById("studdatat").rows.length;
                     for (i = 2; i < rows; i++)
-                        document.getElementById("courset").deleteRow(1);
+                        document.getElementById("studdatat").deleteRow(1);
                     const d = doc.data();
                     addStudentDataTable(doc.id, d.Name, d.CGPA, d.Branch);
                 } else {
