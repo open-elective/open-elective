97bfcb972498767047f60dc1a3649ec8dab175df
niranjangirhe <niranjangirheindia@gmail.com>
2021-06-08 12:33:59 +0530

    done with adminsetting excel

--- scripts/adminsettings.js
+++ scripts/adminsettings.js
@@ -133,22 +133,106 @@ async function deleteallcourse() {
     });
     progress.style.visibility = "hidden";
 }
-function downloadexcel() {
+async function downloadexcel() {
+    var today = new Date();
+    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date + ' ' + time;
     var wb = XLSX.utils.book_new();
     wb.Props = {
-        Title: "SheetJS Tutorial",
-        Subject: "Test",
-        Author: "Red Stapler",
-        CreatedDate: new Date(2017, 12, 19)
+        Title: "Entire Allocation Data",
+        Subject: "Data",
+        Author: "Open-Elective-Devlopers",
+        CreatedDate: new Date(today.getFullYear(), today.getMonth()+1, today.getDate())
     };
-    wb.SheetNames.push("Test Sheet1");
-    wb.SheetNames.push("Test Sheet2");
-    var ws_data = [['hello', 'world']];
+
+    const ref1 = await db.collection("studentData").orderBy("CGPA", "desc");
+    storedatasd = await ref1.get();
+
+    const ref2 = await db.collection("courseData");
+    storedatacd = await ref2.get();
+
+    const ref3 = await db.collection("Schools");
+    storedatass = await ref3.get();
+
+    const ref4 = await db.collection("studentprefs");
+    storedatasp = await ref4.get();
+
+
+    //course data sheet
+    var ws_data = [];
+    ws_data.push(["Course ID","Course Name", "Internal Capacity","Internal Filled","External Capacity", "External Filled", "Course school", "Course Allowed"]);
+    for (i = 0; i < storedatacd.docs.length; i++) {
+        data = storedatacd.docs[i].data()
+        var temp=""
+        if (data.All) {
+            temp = "All"
+        }
+        else {
+            if (data.SCET) {
+                temp = temp + "SCET" + ", "
+            }
+            if (data.SEE) {
+                temp = temp + "SEE" + ", "
+            }
+            if (data.SMCEM) {
+                temp = temp + "SMCEM" + ", "
+            }
+            if (data.SMCEC) {
+                temp = temp + "SMCEC" + ", "
+            }
+            if (data.SCE) {
+                temp = temp + "SCE" + ", "
+            }
+        }
+        ws_data.push([storedatacd.docs[i].id,data.Name, data.InternalCap, data.Intfill, data.Extfill, data.ExternalCap, data.School, temp]);
+    }
     var ws = XLSX.utils.aoa_to_sheet(ws_data);
-    wb.Sheets["Test Sheet1"] = ws;
-    wb.Sheets["Test Sheet2"] = ws;
+    wb.SheetNames.push("Course Data");
+    wb.Sheets["Course Data"] = ws;
+
+
+
+    //student data sheet
+    var ws_data = [];
+    ws_data.push(["PRN", "Name", "CGPA", "School", "Allocation"]);
+    for (i = 0; i < storedatasd.docs.length; i++) {
+        ws_data.push([storedatasd.docs[i].id, storedatasd.docs[i].data().Name, storedatasd.docs[i].data().CGPA, storedatasd.docs[i].data().School, storedatasd.docs[i].data().alloc]);
+    }
+    var ws = XLSX.utils.aoa_to_sheet(ws_data);
+    wb.SheetNames.push("Student Data");
+    wb.Sheets["Student Data"] = ws;
+
+
+
+    //student pref sheet
+    var ws_data = [];
+    ws_data.push(["PRN", "Preferences"]);
+    for (i = 0; i < storedatasp.docs.length; i++) {
+        var myprefstr = storedatasp.docs[i].data().mypref.join(', ');
+        ws_data.push([storedatasp.docs[i].id, myprefstr]);
+    }
+    var ws = XLSX.utils.aoa_to_sheet(ws_data);
+    wb.SheetNames.push("Student Preferences");
+    wb.Sheets["Student Preferences"] = ws;
+
+
+
+    //School sheet
+    var ws_data = [];
+    ws_data.push(["School", "Allowed Course"]);
+    for (i = 0; i < storedatass.docs.length; i++) {
+        var school = storedatass.docs[i].data()[storedatass.docs[i].id].join('# ');
+        ws_data.push([storedatass.docs[i].id,school]);
+    }
+    var ws = XLSX.utils.aoa_to_sheet(ws_data);
+    wb.SheetNames.push("Schools");
+    wb.Sheets["Schools"] = ws;
+
+
+
     var wbout = XLSX.write(wb, { bookType: 'xlsx', type: 'binary' });
-    saveAs(new Blob([s2ab(wbout)], { type: "application/octet-stream" }), 'test.xlsx');
+    saveAs(new Blob([s2ab(wbout)], { type: "application/octet-stream" }), 'EntireData'+dateTime+'.xlsx');
 }
 function s2ab(s) {
     var buf = new ArrayBuffer(s.length); //convert s to arrayBuffer
